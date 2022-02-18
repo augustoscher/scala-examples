9ec19946434b628bf5c75a9b0eaf6bc61a72cf7d
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-02-17 23:36:48 -0300

    feat: scala basics

---
+++ src/main/scala/com/augustoscher/Mapping.scala
@@ -0,0 +1,49 @@
+object JurisprudenceSearchFilter extends DocumentSearchFilter {
+  private val jurisTypeMappings: Map[String, List[String]] = Map(
+    "DECISAO" -> List(
+      JurisprudenciaType.DECISAO.name(),
+      JurisprudenciaType.DELIMITACAO_DE_DECISAO.name(),
+      JurisprudenciaType.DECISAO_DA_PRESIDENCIA.name(),
+      JurisprudenciaType.DECISAO_DA_VICE_PRESIDENCIA.name(),
+      JurisprudenciaType.DECISAO_DA_CORREGEDORIA.name()
+    ),
+    "ACORDAO" -> List(
+      JurisprudenciaType.ACORDAO.name(),
+      JurisprudenciaType.ACORDAO_REPETITIVO.name(),
+      JurisprudenciaType.REPERCUSSAO_GERAL.name(),
+      JurisprudenciaType.ACORDAO_AFETACAO.name(),
+      JurisprudenciaType.INCIDENTE_ASSUNCAO_COMPETENCIA.name()
+    ),
+    "SUMULA"-> List("SUMULA"),
+    "SENTENCA" -> List(
+      JurisprudenciaType.SENTENCA.name()
+    ),
+    "DESPACHO" -> List(
+      JurisprudenciaType.DESPACHO.name()
+    )
+  )
+
+  private def getFilteredTypes(jurisTypes: List[String]): List[String] = {
+    jurisTypes.flatMap(jurisTypeMappings.get).flatten
+  }
+
+  def degree(degree: Option[JurisprudenciaGrau]): Option[Query] = {
+    for (d <- degree) yield {
+      termQuery("grau", d)
+    }
+  }
+
+  def hasDecisionFacts(hasDecisionFacts: Option[Boolean]): Option[Query] = {
+    for (h <- hasDecisionFacts) yield {
+      termQuery("_possuiEmenta", h)
+    }
+  }
+
+  def jurisTypes(jurisTypes: List[String]): Option[Query] = {
+    getFilteredTypes(jurisTypes) match {
+      case Nil => None
+      case _ if jurisTypes.contains("TODOS") => None
+      case types => Some(termsQuery("_jurisType", types))
+    }
+  }
+}
