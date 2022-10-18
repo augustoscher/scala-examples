c5ff9999f6ebbc375001a81da9c7c5700da9f9f2
Augusto <augusto@jusbrasil.com.br>
2022-10-18 17:02:38 -0300

    Update Collections.scala

--- src/main/scala/com/augustoscher/Collections.scala
+++ src/main/scala/com/augustoscher/Collections.scala
@@ -2,16 +2,17 @@ package com.augustoscher
 
 object Collections extends App {
 
-//  array
-val a1 = Array(1, 2, 3, 4, 5, 6)
-println(a1(2)) // 3
+  //  array
+  val a1 = Array(1, 2, 3, 4, 5, 6)
+  println(a1(2)) // 3
 
-// map
-val a2 = a1 map (_ * 3)
+  // map
+  val a2 = a1 map (_ * 3)
 
-// filter
-val a3 = a1.filter(_ % 2 == 0)
-val a4 = a1 filter (_ % 2 == 0)
-
-
+  // filter
+  val a3 = a1.filter(_ % 2 == 0)
+  val a4 = a1 filter (_ % 2 == 0)
+  
+  //exists
+  val res = someList exists { ref: ReferenciaExterna => ref.getOrigem() == OrigemReferenciaExterna.TUXEDO_LAWSUIT }
 }
